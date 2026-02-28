06569ed15a36f1c4a2ddb44459b55e88bf8a0678
Ragnhild Thielemann <ragnhild.thi@gmail.com>
2026-02-28 09:53:11 +0100

    kommentarer i koden

--- scr/covid19.py
+++ scr/covid19.py
@@ -11,9 +11,9 @@ from scr.SEIR0 import SEIR0
 class SEIRimport(SEIR0): #impotrerer SEIR som en subklasse
     def __init__(self,b,r_ia = 0.1 , r_e2 = 1.55, lmda1 = 0.33, lmda2 = 0.5, p_a = 0.4, mu = 0.2,sigma = 10):
       
-        self.sigma = sigma
+        self.sigma = sigma #sigma markeerer at det kommer mennesker utenifra med smitte
         if isinstance(b,(float,int)):
-            self.b_f = lambda t: b
+            self.b_f = lambda t: b #gjør beta-verdien om til en funksjon, dersom den ikke allerede er kallbar
         elif callable(b):
             self.b_f = b
         super().__init__(self.b_f(0),r_ia = r_ia , r_e2 = r_e2 , lmda1 = lmda1, lmda2 = lmda2 , p_a = p_a , mu = mu) #oppdaterer beta-atributten til å bli en kallbar funskjon. alle de andre atributtene er konstante, så de trenger vi ikke å oppdatere
@@ -26,14 +26,14 @@ class SEIRimport(SEIR0): #impotrerer SEIR som en subklasse
 
 def solve_SEIR0(model,T,N,S_0, E2_0,beta):
     u0 = [S_0,0,E2_0,0,0,0]
-    f = SEIRimport(beta) #gir oss derivertverdiene
-    # vi må oppdatere derivertvediene 
+    f = SEIRimport(beta) #gir oss derivertverdiene ved å lgae en instns av subklassen
+    
     f_ = model(f)
     t,u = f_.integrer(u0,T,N)
     return t,u
 
 n = Beta("beta.txt")
 
-t,u = solve_SEIR0(Euler,900,1000,5.5*10**6,100,n) #gir oss løsningen fra den forrgje filen
+t,u = solve_SEIR0(Euler,900,1000,5.5*10**6,100,n) #henter ut verdiene fra SOLVE_SEIR
 
-plot_SEIR(t,u,["Ia","I"])
+plot_SEIR(t,u,["Ia","I"]) #bruker plotfunksjonen til å visualsiere dette
--- scr/lockdown.py
+++ scr/lockdown.py
@@ -6,9 +6,9 @@ from datetime import date, timedelta
 from scr.SEIR import plot_SEIR
 from scr.outbreak import solve_SEIR #gir oss løsningen fra den forrgje filen
 from scr.ODEsolver import Euler
-class Beta:
-    def __init__(self,filename):
-        with open(filename,"r") as fil:
+class Beta: #lager en klasse for å sortere dataen i beta-verdier
+    def __init__(self,filename): #betaverdiene markerer antall personer en smittet person smitter 
+        with open(filename,"r") as fil: #åpner filen
             fil.readline()
             fil.readline()
             n = []
@@ -20,7 +20,7 @@ class Beta:
                 beta.append(float(words[2])) 
                 
             dager = 0
-            d_liste = [] ; b_liste = []
+            d_liste = [] ; b_liste = [] #to lister for å sortere verdiene, en for dager, en for betaverdier
             for k in range(len(n)-1):
                 delta = n[k+1] - n[k]
                 n_days = delta.days
@@ -48,9 +48,17 @@ class Beta:
 
     def plot(self,T):
         t = np.linspace(0,self.d_liste[-1],T)
-        u = (self(t))
-        plt.plot(t,u)
-n = Beta("beta.txt")
+        u = (self(t)) #kjører gjennom klassen, til den finner en kallbar funksjon
+        plt.plot(t,u,label = "Betavalues",color = "hotpink")
+        plt.xlabel("Tid(dager)")
+        plt.grid()
+        plt.ylabel("Verdi for beta")
+        plt.title("Verdier for Beta med reelt datasett")
+        plt.legend()
+
+        plt.show()
+
+n = Beta("beta.txt") #lager en instans av klassen, der vi legger inn filen med verdier for beta
 n.plot(10000)
 
 
